25ad434fd6666fcf40027d5823935c998648fc0a
Tadeáš <tadeastpj@gmail.com>
2026-01-11 09:43:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ const menuBar = document.querySelector("#navigation");
 window.openMenu = () => {
     if (menuBar.style.opacity === "0") {
         menuBar.style.opacity = "1";
+
+        if (visualViewport.width < 665) {
+            document.body.style.overflowY = "hidden";
+        }
     } else {
         menuBar.style.opacity = "0";
+        document.body.style.overflowY = "initial";
     }
 }
 
 if (document.body.id === "index") {
     if (visualViewport.width < 665) {
-
-        document.body.style.backgroundImage = 'url("evaainka nastojato.jpg")';
+        document.body.style.backgroundImage = 'url("mobily nastojato.jpg")';
+    } else {
+        menuBar.style.opacity = "1";
     }
 } 
