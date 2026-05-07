a614c20f2e016c0dd858df02f0bb6877ff51fa01
Tadeáš <tadeastpj@gmail.com>
2026-05-07 09:14:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -32,20 +32,28 @@ if (document.body.id === "player") {
         document.querySelector("video").innerHTML += '<source src="https://www.dropbox.com/scl/fi/lm2svd8myhz9oat9rgm1n/anomalie-film.mp4?rlkey=a5ccci2umrkfo9mc8ffrazuzz&st=o9yhl8jo&raw=1" type="video/mp4"/>';
         document.querySelector("video").poster = "anomalie.webp";
         document.querySelector("#player-film-title").innerHTML = "Anomálie";
-        document.querySelector("#player-film-link").href = "https://dafilms.cz/film/10696-anomalie"
-        document.querySelector("#player-film-link").innerHTML = "K vzhlédnutí také na dafilms.cz"
+        document.querySelector("#player-film-link").href = "https://dafilms.cz/film/10696-anomalie";
+        document.querySelector("#player-film-link").innerHTML = "K vzhlédnutí také na dafilms.cz";
     } else if (film === "arbor-vitae") {
         document.querySelector("video").innerHTML += '<source src="https://www.dropbox.com/scl/fi/nl14g5iycfoxwhi388p09/arbor-vitae.mp4?rlkey=qx7274x68plqrlfqmpvit77dk&st=6mb2mjhg&raw=1" type="video/mp4"/>';
         document.querySelector("video").poster = "arbor vitae.png";
         document.querySelector("#player-film-title").innerHTML = "Arbor Vitae";
     } else if (film === "asexualove") {
-        document.querySelector("video").innerHTML += '<source src="https://drive.google.com/file/d/1Ngd7kD1NyGHF1fLFDz9azfvZz2OGKbOQ/preview';
-        document.querySelector("video").poster = "asexualove.webp";
-        document.querySelector("#player-film-title").innerHTML = "AsexuaLOVE";
-        document.querySelector("#player-film-link").href = "https://dafilms.cz/film/10486-asexualove"
-        document.querySelector("#player-film-link").innerHTML = "K vzhlédnutí také na dafilms.cz"
-    } else {
-        window.location.href = "https://lammelova.com/films"
+        document.body.innerHTML =  `
+        <iframe src="https://drive.google.com/file/d/1Ngd7kD1NyGHF1fLFDz9azfvZz2OGKbOQ/preview" width="640" height="400" allow="autoplay"></iframe>
+        <br><br>
+        <h4 id="player-film-title">AsexuaLOVE</h4>
+        <a id="player-film-link" href="https://dafilms.cz/film/10486-asexualove">K vzhlédnutí také na dafilms.cz</a>
+        `
+    } else if (film === "v-atelieru") {
+        document.body.innerHTML =  `
+        <iframe src="https://drive.google.com/file/d/1zXKOOvmL_py-6tczrGawPIUeLBQ9EB-P/preview" width="640" height="400" allow="autoplay"></iframe>
+        <br><br>
+        <h4 id="player-film-title">V ateliéru</h4>
+        `
+    }
+    else {
+        window.location.href = "https://lammelova.com/films";
     }
 }
 
